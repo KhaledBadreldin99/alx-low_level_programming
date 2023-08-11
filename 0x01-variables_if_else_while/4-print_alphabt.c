c70d2c295227ae08ecc78c580fb000a40ccc41c3
Khaled Elswaf <Khaled.badr24.kb@gmail.com>
2023-08-11 09:45:03 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	for (char c = 'a'; c <= 'z'; ++c)
+		if (c != 'q' && c != 'e')
+			putchar(c);
+	putchar('\n');
+	return (0);
+}
