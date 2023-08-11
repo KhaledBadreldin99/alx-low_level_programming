58ea4379f01461d7961b9ade5ffb583aea1415f7
Khaled Elswaf <Khaled.badr24.kb@gmail.com>
2023-08-11 11:55:19 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * REturn: 0 (success)
+*/
+int main(void)
+{
+	char ch = 'z';
+
+	while (ch >= 'a')
+	{
+		putchar(ch);
+		ch--;
+	}
+	putchar('\n');
+	return (0);
+}
